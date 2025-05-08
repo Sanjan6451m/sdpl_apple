743f0e0c54d4d96ed10cc628be629a19921b27f6
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-05-08 11:03:54 +0530

    UI changes

--- src/app/components/pages/homepage/homepage.component.ts
+++ src/app/components/pages/homepage/homepage.component.ts
@@ -78,6 +78,10 @@ newsEvents = [
 }
 
   ngOnInit() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css';
+    document.head.appendChild(link);
     this.initializeCounters();
   }
 

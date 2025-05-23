f4dfbb0df4ed4316e87004e270b006e53ef7beea
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-05-23 11:51:12 +0530

    iPad Air

--- src/app/components/pages/iPad/i-pad-air-new/i-pad-air-new.component.ts
+++ src/app/components/pages/iPad/i-pad-air-new/i-pad-air-new.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-i-pad-air-new',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './i-pad-air-new.component.scss'
 })
 export class IPadAirNewComponent {
+
+  iPadAirUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.iPadAirUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'assets/embedded/iPadAir/index.html'
+    );
+  }
 
 }

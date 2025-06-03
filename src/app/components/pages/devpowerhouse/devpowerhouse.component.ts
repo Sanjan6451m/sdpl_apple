ec2bd995d04fd81ab41430ae8c86878a448c875a
Diwakar0793 <54579283+Diwakar0793@users.noreply.github.com>
2025-06-03 12:54:23 +0530

    routing

--- src/app/components/pages/devpowerhouse/devpowerhouse.component.ts
+++ src/app/components/pages/devpowerhouse/devpowerhouse.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-devpowerhouse',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './devpowerhouse.component.html',
   styleUrl: './devpowerhouse.component.scss'
 })
-export class DevpowerhouseComponent {
+export class DevpowerhouseComponent implements OnInit{
+
+  constructor(private router: Router) { }
+  
+  ngOnInit(): void {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css';
+    document.head.appendChild(link);
+  }
 
 }
